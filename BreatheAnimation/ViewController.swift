f553428a225c9e06d1d62fd62a23dc4665e315e6
Venky Venkatakrishnan <eeshwar1@gmail.com>
2021-09-25 09:23:33 -0500

    Initial Commit

---
+++ BreatheAnimation/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BreatheAnimation
+//
+//  Created by Venkateswaran Venkatakrishnan on 9/25/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
